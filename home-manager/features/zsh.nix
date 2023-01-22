97b0730d0339aa53403f5450394f60d00ead8d74
Dennis Merkert <info@dmerkert.de>
2023-01-22 20:45:33 +0000

    fix bug with git in zsh

--- home-manager/features/zsh.nix
+++ home-manager/features/zsh.nix
@@ -12,5 +12,11 @@
         sw_heureka = ''sudo nixos-rebuild switch --flake ".#heureka"'';
         hm_heureka = ''home-manager switch --flake ".#user@heureka"'';
       };
+      initExtra = ''
+        # Makes git auto completion faster favouring for local completions
+        __git_files () {
+          _wanted files expl 'local files' _files
+        }
+      ''
     };
 }
